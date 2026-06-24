1e9badca4cd5d06317154e8f0bf01719fe5fad3f
thiagocghc <50628917+thiagocghc@users.noreply.github.com>
2026-06-24 09:30:43 -0400

    salvar

---
+++ cadastro-fornecedor.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo "TELA DO FORNECEDOR"; ?> </h1>
+
+    <main>
+        <div>
+            <p>card do produto</p>
+        </div>
+    </main>
+
+
+    <footer>
+        <span>foot</span>
+    </footer>
+</body>
+</html>
